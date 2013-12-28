4e494a0bc18a1fc56ebb508e97d0d63ba1791e98
smartlib <smartslib@gmail.com>
2013-12-28 18:05:21 +0800

    初始化项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'c20777feac1503347dd0198d109133f27a776e8bfa123832834f69a7ac86a7116e4720ff4e7ab50831ddf21e60a7053132663eb1fc4c5268094c623b9dd51bc6'
